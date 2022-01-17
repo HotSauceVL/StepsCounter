dac68b78e9f0ffcbc482f15e90f6b7e039dd86db
Vladimir <vova030494@gmail.com>
2022-01-18 02:08:50 +1000

    Счетчик шагов работает

--- src/Converter.java
+++ src/Converter.java
@@ -1,10 +1,12 @@
 public class Converter {
 
     double convertToDistanseKm(int steps){
+
         return steps * 75 / 100000;
     }
 
     double convertToKKallories(int steps){
+
         return steps * 50 / 1000;
     }
 }
